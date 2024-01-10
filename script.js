83bc62dde9329206f33bf9a1f82e0252c0ccc168
Abdinasir Ali <131626624+abdinasir5@users.noreply.github.com>
2024-01-09 22:49:28 -0500

    display functionality

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const addBookBtn = document.getElementById('addbook-btn');
 const submitBtn = document.getElementById('submit-btn');
 const closeFormBtn = document.getElementById('close-form-btn');
 
+const library = document.getElementById('books');
 const form = document.getElementById('book-form');
 
 const myLibrary = [];
@@ -11,6 +12,63 @@ function Book(title, author, pages, isRead) {
     this.author = author;
     this.pages = pages;
     this.isRead = isRead;
+}
+
+function addBook() {
+    let title = form.querySelector('#title').value;
+    let author = form.querySelector('#author').value;
+    let pages = parseInt(form.querySelector('#pages').value);
+    let readStatus = form.querySelector('#read-status').checked;
+    let book = null;
+    // console.log(typeof title, typeof author, typeof pages, typeof readStatus);
+
+    // Create New Book
+    if (title && author && pages && readStatus) {
+        book = new Book(title, author, pages, readStatus);
+    
+        // Push To Library
+        myLibrary.push(book);
+
+        // console.log(myLibrary);
+
+        form.classList.toggle('active');
+        updateDisplay();
+    }
+}
+
+function createCard(bookObj) {
+    let bookCard = document.createElement('div');
+    bookCard.classList.add('book-card');
+
+    bookCard.innerHTML = `
+    <div class="card-details">
+        <h2 id="book-title">${bookObj.title}</h2>
+        <h3 id="book-author">by ${bookObj.author}</h3>
+        <p id="book-pages">${bookObj.pages} pages</p>
+        <div class="card-edit">
+            <label class="card-status">
+            <input type="checkbox"> Mark as read</label>
+            <button class="delete-btn">
+                <i class="fa-solid fa-trash"></i>
+            </button>               
+        </div>
+    </div>`
+    return bookCard;
+}
+
+function updateDisplay() {
+    // Empty Out Current Display
+    const display = document.getElementById('books');
+    display.innerHTML = '';
+
+    myLibrary.forEach((book) => {
+        // Create New Book Card
+        const bookCard = createCard(book);
+
+        // Add Book To Display
+        display.appendChild(bookCard);
+        // console.log(book.title, book.author, book.pages, book.isRead);
+    })
 }
 
 addBookBtn.addEventListener('click', () => {
@@ -23,9 +81,5 @@ closeFormBtn.addEventListener('click', () => {
 
 submitBtn.addEventListener('click', (event) => {
     event.preventDefault();
-    let title = form.querySelector('#title').value;
-    let author = form.querySelector('#author').value;
-    let pages = parseInt(form.querySelector('#pages').value);
-    let readStatus = form.querySelector('#read-status').checked;
-    console.log(typeof title, typeof author, typeof pages, typeof readStatus);
+    addBook();
 })
